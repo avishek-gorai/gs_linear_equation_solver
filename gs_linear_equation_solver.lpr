d6f00b46ed844ed66322b4eeb62db2d65885efd1
Avishek Gorai <avishekgorai@myyahoo.com>
2025-05-25 21:09:24 +0000

    Made it full fledge command line application and added testing harness.

---
+++ gs_linear_equation_solver.lpr
@@ -0,0 +1,77 @@
+PROGRAM gs_linear_equation_solver;
+
+USES
+    gs_linear_equation_solver_functions,
+    Classes,
+    SysUtils,
+    CustApp;
+
+TYPE
+    GSLinearEquationSolver = CLASS(TCustomapplication)
+    PROTECTED
+        PROCEDURE doRun;
+        OVERRIDE;
+
+    PUBLIC
+        CONSTRUCTOR create(the_owner : TComponent);
+        OVERRIDE;
+
+        DESTRUCTOR destroy;
+        OVERRIDE;
+
+        FUNCTION writeHelp : GSLinearEquationSolver;
+        VIRTUAL;
+    END;
+
+    PROCEDURE GSLinearEquationSolver.doRun;
+
+    VAR
+        error_message : string;
+
+    BEGIN
+        error_message := self.checkOptions('h', 'help');
+        IF error_message <> '' THEN
+        BEGIN
+            self.showException(Exception.create(error_message));
+            self.terminate
+        END;
+
+        IF self.hasOption('h', 'help') THEN
+        BEGIN
+            self.writeHelp;
+            self.terminate
+        END;
+
+        self.terminate
+    END;
+
+    CONSTRUCTOR GSLinearEquationSolver.create(the_owner : TComponent);
+
+    BEGIN
+      INHERITED create(the_owner);
+      self.stopOnException := true
+    END;
+
+    DESTRUCTOR GSLinearEquationSolver.destroy;
+
+    BEGIN
+      INHERITED destroy;
+    END;
+
+    FUNCTION GSLinearEquationSolver.writeHelp : GSLinearEquationSolver;
+
+    BEGIN
+        writeln('Usage: ', self.exeName, ' -h');
+        writeHelp := self
+    END;
+
+VAR
+    application : GSLinearEquationSolver;
+
+BEGIN
+    application := GSLinearEquationSolver.create(NIL);
+    application.title := 'Gauss-Seidel Linear Equation Solver';
+    application.run;
+    application.free
+END.
+
---
+++ gs_linear_equation_solver_functions.pas
@@ -0,0 +1,90 @@
+UNIT gs_linear_equation_solver_functions;
+
+INTERFACE
+    CONST
+        maximum_variables = 30;
+
+    TYPE
+        matrix = ARRAY [1 .. maximum_variables, 1 .. maximum_variables] OF real;
+        natural_number = 1 .. maxint;
+        standard_array = ARRAY [1 .. maximum_variables] OF real;
+        diagonal_dominance_result = RECORD
+            equation_number : natural_number;
+            test : boolean;
+        END;
+
+    FUNCTION diagonal_dominance(coefficient_matrix : matrix) : diagonal_dominance_result;
+
+    FUNCTION calculate(coefficient_matrix : matrix; constants : standard_array) : standard_array;
+
+IMPLEMENTATION
+    USES
+        Classes;
+
+    CONST
+        tolerance = 1.0E-6;
+
+    FUNCTION diagonal_dominance(coefficient_matrix : matrix) : diagonal_dominance_result;
+
+    VAR
+        row, column                                : integer;
+        sum_of_first_half, sum_of_second_half, sum : real;
+        answer                                     : diagonal_dominance_result;
+
+    BEGIN
+        answer.test := true;
+        answer.equation_number := 1;
+
+        row := 1;
+        WHILE (answer.test = true) AND (row <= number_of_variables) DO
+        BEGIN
+              sum_of_first_half := 0.0;
+              FOR column := 1 TO row - 1 DO
+              BEGIN
+                    sum_of_first_half := sum_of_first_half + abs(coefficient_matrix[row, column])
+              END;
+
+              sum_of_second_half := 0.0;
+              FOR column := row + 1 TO number_of_variables DO
+              BEGIN
+                    sum_of_second_half := sum_of_second_half + abs(coefficient_matrix[row, column])
+              END;
+
+              sum := sum_of_first_half + sum_of_second_half;
+
+              answer.test := answer.test AND (abs(coefficient_matrix[row, row]) > sum);
+              row := row + 1
+          END;
+
+        answer.equation_number := row;
+        diagonal_dominance := answer
+    END;
+
+    FUNCTION calculate(coefficient_matrix : matrix; constants : standard_array) : standard_array;
+
+    VAR
+        index, column, row : integer;
+        accurate : boolean;
+
+    BEGIN
+        FOR index := 1 TO number_of_variables DO
+        BEGIN
+            x[index] := random
+        END;
+
+        accurate := false;
+        WHILE NOT accurate DO
+        BEGIN
+            FOR column := 1 TO index - 1 DO
+            BEGIN
+                sum_of_first_half := sum_of_first_half + coefficient_matrix[row, column] * x[column];
+            END;
+
+            FOR index := 1 TO number_of_variables DO
+            BEGIN
+                x_new[index] := (b[index] - sum_of_first_half - sum_of_second_half) / coefficient_matrix[index, index]
+            END
+        END
+    END;
+END.
+
